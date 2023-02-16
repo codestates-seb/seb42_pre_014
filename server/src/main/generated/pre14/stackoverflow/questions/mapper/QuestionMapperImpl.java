d87494d63e60fd2129c9701c3fdd0bd2c4ea5022
kjsu1994 <kjsu1994@gmail.com>
2023-02-16 15:59:16 +0900

    a

---
+++ server/src/main/generated/pre14/stackoverflow/questions/mapper/QuestionMapperImpl.java
@@ -0,0 +1,29 @@
+package pre14.stackoverflow.questions.mapper;
+
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+import pre14.stackoverflow.questions.dto.QuestionDto;
+import pre14.stackoverflow.questions.entity.Question;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-02-16T15:57:50+0900",
+    comments = "version: 1.5.1.Final, compiler: javac, environment: Java 11.0.17 (Azul Systems, Inc.)"
+)
+@Component
+public class QuestionMapperImpl implements QuestionMapper {
+
+    @Override
+    public Question questionPostDtoToQuestion(QuestionDto.Post requestBody) {
+        if ( requestBody == null ) {
+            return null;
+        }
+
+        Question question = new Question();
+
+        question.setTitle( requestBody.getTitle() );
+        question.setBody( requestBody.getBody() );
+
+        return question;
+    }
+}
