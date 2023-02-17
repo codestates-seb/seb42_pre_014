7bcafcb6214013f9b5a91e010776a24722a5e628
ok-soosoo <doub22e@gmail.com>
2023-02-17 11:26:21 +0900

    메인페이지 CSS, json-server 적용

--- frontend/src/QuestionPage.js
+++ frontend/src/QuestionPage.js
@@ -2,29 +2,44 @@ import styled from "styled-components";
 import QuestionRow from "./QuestionRow";
 import Header1 from "./Header1";
 import BlueButtonLink from "./BlueButtonLink";
-
+import { useState, useEffect } from "react";
 const HeaderRow = styled.div`
-    display: grid;
-    grid-template-columns: 1fr min-content;
-    padding: 30px 20px;
+  display: grid;
+  grid-template-columns: 1fr min-content;
+  padding: 30px 20px;
 `;
 
 function QuestionsPage() {
-    return (
-        <div>
-            <HeaderRow>
-                <Header1 style={{ margin: 0 }}>Top Questions</Header1>
-                <BlueButtonLink to="./ask">Ask&nbsp;Question</BlueButtonLink>
-            </HeaderRow>
-            <QuestionRow />
-            <QuestionRow />
-            <QuestionRow />
-            <QuestionRow />
-            <QuestionRow />
-            <QuestionRow />
-            <QuestionRow />
-        </div>
-    )
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/questions")
+      .then((res) => res.json())
+      .then((json) => setQuestions(json))
+      .catch((err) => console.err(err));
+  }, []);
+  console.log(questions);
+
+  return (
+    <div>
+      <HeaderRow>
+        <Header1 style={{ margin: 0 }}>Top Questions</Header1>
+        <BlueButtonLink to="./ask">Ask&nbsp;Question</BlueButtonLink>
+      </HeaderRow>
+      {questions.map((el) => {
+        return (
+          <QuestionRow
+            votes={el.votes}
+            answers={el.answers}
+            tags={el.tags}
+            title={el.title}
+            views={el.views}
+            key={el.id}
+          />
+        );
+      })}
+    </div>
+  );
 }
 
 export default QuestionsPage;
--- frontend/src/QuestionRow.js
+++ frontend/src/QuestionRow.js
@@ -1,73 +1,89 @@
 import styled from "styled-components";
 
-const QuestionStat = styled.div`
-    text-align: center;
-    display: inline-block;
-    font-size: 1.2rem;
-    color: #aaa;
-    margin-top: 7px;
-    span{
-        font-size: .7rem;
-        display: block;
-        font-weight: 300;
-        margin-top: 4px;
-    }
+const QuestionStat_container = styled.div`
+  margin-left: 30px;
+`;
+const QuestionStat = styled.span`
+  align-self: stretch;
+  text-align: center;
+  display: inline-block;
+  font-size: 0.8rem;
+  color: #aaa;
+  margin: 3px 0 3px 0;
+  span {
+    font-size: 0.7rem;
+    font-weight: 300;
+    text-align: end;
+  }
 `;
 const QuestionTitleArea = styled.div`
-    padding: 0 30px;
+  padding: 0 10px;
 `;
 const Tag = styled.span`
-    display: inline-block;
-    margin-right: 5px;
-    background-color: #3e4a52;
-    color: #9cc3db;
-    padding: 7px;
-    border-radius: 4px;
-    font-size: .9rem;
+  display: inline-block;
+  margin-right: 5px;
+  background-color: #3e4a52;
+  color: #9cc3db;
+  padding: 7px;
+  border-radius: 4px;
+  font-size: 0.7rem;
 `;
 const QuestionLink = styled.a`
-    text-decoration: none;
-    color: #3ca4ff;
-    font-size: 1.1rem;
-    display: block;
-    margin-bottom: 5px;
+  text-decoration: none;
+  color: #3ca4ff;
+  font-size: 1.1rem;
+  display: block;
+  margin-bottom: 5px;
 `;
 const StyledQuestionRow = styled.div`
-    background-color: rgba(255, 255, 255, .05);
-    padding: 15px 15px 10px;
-    display: grid;
-    grid-template-columns: repeat(3, 50px) 1fr;
-    border-top: 1px solid #555;
+  background-color: rgba(255, 255, 255, 0.05);
+  padding: 15px 15px 10px;
+  display: grid;
+  grid-template-columns: 85px 1fr;
+  /* grid-template-rows: repeat(3, 20px); */
+  grid-template-rows: 1fr;
+  border-top: 1px solid #555;
 `;
 const WhoAndWhen = styled.div`
-    display: inline-block;
-    color: #aaa;
-    font-size: .8rem;
-    float: right;
-    padding: 10px 0;
+  display: inline-block;
+  color: #aaa;
+  font-size: 0.8rem;
+  float: right;
+  padding: 10px 0;
 `;
 const UserLink = styled.a`
-    color: #3ca4ff;
+  color: #3ca4ff;
 `;
 
-function QuestionRow() {
-    return (
-        <StyledQuestionRow>
-            <QuestionStat>0<span>votes</span></QuestionStat>
-            <QuestionStat>1<span>answers</span></QuestionStat>
-            <QuestionStat>6<span>views</span></QuestionStat>
-            <QuestionTitleArea>
-                <QuestionLink>Getting string in quotes in javascript</QuestionLink>
-                <Tag>javascript</Tag>
-                <WhoAndWhen>
-                    asked 2mins ago <UserLink></UserLink>
-                </WhoAndWhen>
-                <Tag>parsing</Tag>
-                <Tag>quetes</Tag>
-                <Tag>literals</Tag>
-            </QuestionTitleArea>
-        </StyledQuestionRow>
-    );
+function QuestionRow({ votes, answers, tags, title, views }) {
+  return (
+    <StyledQuestionRow>
+      <QuestionStat_container>
+        <QuestionStat>
+          {votes}
+          <span> votes</span>
+        </QuestionStat>
+        <QuestionStat>
+          {answers}
+          <span> answers</span>
+        </QuestionStat>
+        <QuestionStat>
+          {views}
+          <span> views</span>
+        </QuestionStat>
+      </QuestionStat_container>
+
+      <QuestionTitleArea>
+        <QuestionLink>{title}</QuestionLink>
+        <WhoAndWhen>
+          asked 2mins ago <UserLink></UserLink>
+        </WhoAndWhen>
+        {tags.map((el) => {
+          return <Tag>{el}</Tag>;
+        })}
+      </QuestionTitleArea>
+    </StyledQuestionRow>
+  );
 }
 
 export default QuestionRow;
