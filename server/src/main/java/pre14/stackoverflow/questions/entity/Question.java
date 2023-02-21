f942da8920cc2825071a98faf304619bc2ffcafa
tjrwls9881 <k37629881@gmail.com>
2023-02-21 14:09:30 +0900

    병합

--- server/src/main/java/pre14/stackoverflow/questions/entity/Question.java
+++ server/src/main/java/pre14/stackoverflow/questions/entity/Question.java
@@ -16,7 +16,7 @@ import java.time.LocalDateTime;
 @RequiredArgsConstructor //아무것도 없이 만들어줌/ 편안
 @EntityListeners(AuditingEntityListener.class)//시간 생성해주는 것 LocalDateTime함께사용
 @ToString //애플리케이션 실행 시 콘솔에 여러정보 확인할 수 있게해주는것
-@Table(name = "question")
+@Table(name = "squestion")
 public class Question {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
