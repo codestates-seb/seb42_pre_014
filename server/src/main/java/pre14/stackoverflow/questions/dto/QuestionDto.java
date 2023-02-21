23744b0756dbe487cd6131ff8aaa8c08424992a8
kjsu1994 <kjsu1994@gmail.com>
2023-02-21 09:20:52 +0900

    validate

--- server/src/main/java/pre14/stackoverflow/questions/dto/QuestionDto.java
+++ server/src/main/java/pre14/stackoverflow/questions/dto/QuestionDto.java
@@ -5,6 +5,7 @@ import pre14.stackoverflow.questions.entity.Question;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
 
@@ -15,7 +16,9 @@ public class QuestionDto {
     public static class Post{
         private Long questionId;
         @NotBlank(message = "제목을 작성해주세요")
+        @Size(max = 100, message = "100자 이내로 작성해 주세요.")
         private String title;
+        @Size(min = 10, max = 5000, message = "10자 이상, 5000자 이내로 작성해 주세요.")
         @NotBlank(message = "본문을 작성해주세요")
         private String contents;
     }
@@ -25,9 +28,12 @@ public class QuestionDto {
     @ToString
     public static class Patch{
         private Long questionId;
+        @Size(max = 100)
         private String title;
+        @Size(min = 10, max = 5000)
         private String contents;
     }
+
     @Getter
     @Setter
     @RequiredArgsConstructor
--- server/src/main/java/pre14/stackoverflow/questions/entity/Question.java
+++ server/src/main/java/pre14/stackoverflow/questions/entity/Question.java
@@ -21,7 +21,7 @@ public class Question {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long questionId;
-    @Column(nullable = false)
+    @Column(nullable = false, length = 100)
     private String title;
     @Column(nullable = false, length = 5000)
     private String contents;
