524465787f6abe7c8831b235e2dbc7716a28a852
sungstii <sungstii12@gmail.com>
2023-02-28 15:39:56 +0900

    answer 투표 기능 추가

--- server/src/main/java/pre14/stackoverflow/answer/controller/AnswerController.java
+++ server/src/main/java/pre14/stackoverflow/answer/controller/AnswerController.java
@@ -1,26 +1,25 @@
 package pre14.stackoverflow.answer.controller;
 
 import lombok.RequiredArgsConstructor;
-import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import pre14.stackoverflow.answer.dto.AnswerPatchDto;
-import pre14.stackoverflow.answer.dto.AnswerPostDto;
-import pre14.stackoverflow.answer.dto.AnswerResponseDto;
+import pre14.stackoverflow.answer.dto.AnswerDto;
+import pre14.stackoverflow.answer.dto.AnswerVoteDto;
 import pre14.stackoverflow.answer.entity.Answer;
 import pre14.stackoverflow.answer.mapper.AnswerMapper;
-import pre14.stackoverflow.answer.repository.AnswerRepository;
 import pre14.stackoverflow.answer.service.AnswerService;
+import pre14.stackoverflow.answer.service.AnswerVoteService;
 import pre14.stackoverflow.globaldto.MultiResponseDto;
 import pre14.stackoverflow.globaldto.SingleResponseDto;
 import pre14.stackoverflow.member.entity.Member;
 import pre14.stackoverflow.member.mapper.MemberMapper;
 import pre14.stackoverflow.member.service.MemberService;
 import pre14.stackoverflow.questions.entity.Question;
+import pre14.stackoverflow.questions.mapper.QuestionMapper;
 import pre14.stackoverflow.questions.repository.QuestionRepository;
 import pre14.stackoverflow.questions.service.QuestionService;
 
@@ -34,16 +33,18 @@ import java.util.List;
 @RequiredArgsConstructor
 @Validated
 public class AnswerController {
-    private final QuestionRepository questionRepository;
     private final AnswerService answerService;
     private final AnswerMapper mapper;
+    private final QuestionMapper questionMapper;
+    private final AnswerVoteService answerVoteService;
     private final QuestionService questionService;
     private final MemberService memberService;
     private final MemberMapper memberMapper;
 
+
     //post
     @PostMapping
-    public ResponseEntity postAnswer(@RequestBody AnswerPostDto answerPostDto) {
+    public ResponseEntity postAnswer(@Valid @RequestBody AnswerDto.Post answerPostDto) {
 
         Member member = memberService.findVerifiedMember(answerPostDto.getMemberId()); //1번방법
         Question question = questionService.findQuestionById(answerPostDto.getQuestionId());
@@ -65,8 +66,8 @@ public class AnswerController {
     }
     //patch
     @PatchMapping("/{answer-id}")
-    public ResponseEntity patchAnswer(@PathVariable("answer-id") @Positive long answerId,
-                                      @Valid @RequestBody AnswerPatchDto answerPatchDto) {
+    public ResponseEntity patchAnswer(@Positive @PathVariable("answer-id") long answerId,
+                                      @Valid @RequestBody AnswerDto.Patch answerPatchDto) {
         answerPatchDto.setAnswerId(answerId);
         Answer answer = answerService.updateAnswer(mapper.answerPatchDtoToAnswer(answerPatchDto));
         return new ResponseEntity<>(
@@ -101,5 +102,24 @@ public class AnswerController {
     public ResponseEntity deleteAnswer(@PathVariable("answer-id") @Positive long answerId) {
         answerService.deleteAnswer(answerId);
         return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
+    // 답변 추천
+    @PatchMapping("/{answer-id}/upvotes")
+    public ResponseEntity upVoteAnswer(@Positive @PathVariable("answer-id") long voteId,
+                                       @Valid @RequestBody AnswerVoteDto requestBody) {
+
+        Answer votedAnswer = answerVoteService.upVote(voteId, mapper.answerVoteDtoToAnswerVote(requestBody));
+
+        return new ResponseEntity<>(mapper.answerToAnswerResponse(votedAnswer), HttpStatus.OK);
+    }
+
+    // 답변 비추천
+    @PatchMapping("/{answer-id}/downvotes")
+    public ResponseEntity downVoteAnswer(@PathVariable("answer-id") long voteId,
+                                         @Valid @RequestBody AnswerVoteDto requestBody) {
+        Answer votedAnswer = answerVoteService.downVote(voteId, mapper.answerVoteDtoToAnswerVote(requestBody));
+
+        return new ResponseEntity<>(mapper.answerToAnswerResponse(votedAnswer), HttpStatus.OK);
     }
 }
